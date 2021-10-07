7ac10f1ac4bad07fcc1f9216b21e62c2e09816bd
RaphaelAzev <raphaelazev@hotmail.com>
2021-10-07 19:03:37 -0300

    Update test_unidade.py

--- test/test_unidade.py
+++ test/test_unidade.py
@@ -20,12 +20,14 @@ def test_correct_submission():
         event_test["code"] = des.read()
     assert len(lambda_handler(event_test)) == 0
 
+def test_wrong_function():
+    with open('./desafios/desafio1_SemFunc.py','r') as des:
+        event_test["code"] = des.read()
+    answer = lambda_handler(event_test)
+    assert (answer == "Nome da função inválido. Usar 'def desafio{0}(...)'".format(event_test["ndes"]))
 
 def test_incorrect_submission():
     with open('./desafios/desafio1_Incorreto.py','r') as des:
         event_test["code"] = des.read()
-    
-
-# def test_three():
-#     with open('./desafios/desafioSemFunc.py','r') as des3:
-#         desafio3 = des3.read()
+    answer = lambda_handler(event_test)
+    assert (answer == " ".join(event_test["diag"]))
