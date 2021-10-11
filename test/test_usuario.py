be4ac0eb9732f86955038c6ef01b946cbe3ccce7
gabiuas <gabiuas1@Gmail.com>
2021-10-11 18:14:02 -0300

    Changing port from 80 to 8080

--- test/test_usuario.py
+++ test/test_usuario.py
@@ -8,7 +8,7 @@ driver = webdriver.Firefox()
 # Testa o login admin admin no get e ve se funcion
 def test_login_success():
     try:
-        driver.get("admin:admin@localhost:80")
+        driver.get("admin:admin@localhost:8080")
         assert True
     except:
         assert False
@@ -17,7 +17,7 @@ def test_login_success():
 # Testa errar a senha e ve se não funciona
 def test_login_fail():
     try:
-        driver.get("admin:adaim@localhost:80")
+        driver.get("admin:adaim@localhost:8080")
         assert False
     except:
         assert True
@@ -25,7 +25,7 @@ def test_login_fail():
 
 def test_desafio_correto():
     try:
-        driver.get("admin:admin@localhost:80")
+        driver.get("admin:admin@localhost:8080")
         filesend = driver.find_element_by_id("resposta")
         filesend.send_keys(os.getcwd() + "/desafios/desafio1_Correto.py")
         driver.find_element_by_class_name("btn-primary").click()
@@ -42,7 +42,7 @@ def test_desafio_correto():
 
 def test_desafio_incorreto():
     try:
-        driver.get("admin:admin@localhost:80")
+        driver.get("admin:admin@localhost:8080")
         filesend = driver.find_element_by_id("resposta")
         filesend.send_keys(os.getcwd() + "/desafios/desafio1_Incorreto.py")
         driver.find_element_by_class_name("btn-primary").click()
